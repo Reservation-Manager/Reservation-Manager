de0cb2f6de72e5f6051a4e9aecfa6ef2cbb49afc
Aneliya05 <89069514+Aneliya05@users.noreply.github.com>
2024-03-25 23:24:27 +0200

    batch file added and database seeding
    i have not deleted the design changes; dw

--- YourPlace/YourPlace/Program.cs
+++ YourPlace/YourPlace/Program.cs
@@ -68,6 +68,13 @@ public class Program
         
         var app = builder.Build();
 
+        using (var scope = app.Services.CreateScope())
+        {
+            var services = scope.ServiceProvider;
+            var dbContext = services.GetRequiredService<YourPlaceDbContext>();
+            dbContext.Database.Migrate();
+        }
+
         // Configure the HTTP request pipeline.
         if (!app.Environment.IsDevelopment())
         {
